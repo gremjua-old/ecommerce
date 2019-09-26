390707d8d71c57534fba1d8d67f98f60513cc5f4
Juan Gremes <gremjua@gmail.com>
2019-09-26 20:59:29 -0300

    Agregue detalleDePRoducto.php

---
+++ detalleDeProducto.php
@@ -0,0 +1,40 @@
+<?php
+?>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Detalle de producto</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+        <header class="main-header">
+
+        </header>
+        <main>
+            <!-- Imagen -->
+            <section>
+
+            </section>
+            <!-- Nombre y precio -->
+            <section>
+
+            </section>
+            <!-- Descripción -->
+            <section>
+
+            </section>
+            <!-- Botón de compra -->
+            <section>
+
+            </section>
+        </main>
+
+        <footer class="main-footer">
+
+        </footer>
+    </body>
+</html>
