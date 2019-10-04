3136d7e7631601aef20b010f80eda408906570e2
Juan Gremes <gremjua@gmail.com>
2019-10-04 08:21:51 -0300

    Agregue bootstrapLink y header y footer a directorio common.

---
+++ common/header.php
@@ -0,0 +1,10 @@
+<?php
+
+$secciones = [
+    ['nombre' => 'Home', 'url' => '../home.html'],
+    ['nombre' => 'Productos', 'url' => '../listaDeProductos.html'],
+    ['nombre' => 'FAQ', 'url' => '../faq.html'],
+    ['nombre' => 'Contacto', 'url' => '../formularioDeContacto.php']
+  ];
+
+?>
---
+++ detalleDeProducto.php
@@ -0,0 +1,46 @@
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Detalle de producto</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/estilos-detalleDeProducto.css">
+        <?php require_once("./common/bootstrapLink.php")?>
+        <script src="https://use.fontawesome.com/65f6f30092.js"></script>
+    </head>
+    <body>
+        <header class="main-header">
+            HEADER
+        </header>
+        <main>
+            <!-- Imagen -->
+            <section class="product-section">
+                <img class="photo" src="img/img-producto-1.jpg" alt="Producto">
+            </section>
+            <!-- Nombre y precio -->
+            <section class="product-section summary">
+                <h1>Zapatillas negras marca sarasa con lalal y par de medias de regalo</h1>
+                <h2>$249.99</h2>
+            </section>
+            <!-- Descripción -->
+            <section class="product-section description">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur minus ipsum ullam assumenda magnam explicabo blanditiis aliquam quos sunt tempore libero, repellendus accusamus eum. Fugit blanditiis ab similique ea totam?
+                    <br><br>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, temporibus repellat? Recusandae possimus ut, consectetur necessitatibus numquam, nemo ratione impedit sunt eos, cum et magnam cupiditate nulla mollitia aspernatur fuga?
+                    <br><br>Aliquip elit irure officia fugiat eu sunt eu officia excepteur esse consequat non. Nostrud magna ad reprehenderit ut qui labore id. Eu ut dolore deserunt culpa voluptate ipsum duis. Laboris anim do do ex laboris ea aliqua Lorem ea ex. Ullamco velit proident incididunt nulla enim. Officia fugiat ullamco est et veniam aliqua. Lorem cillum dolore consequat laborum id sint qui sit labore excepteur incididunt.
+                </p>
+
+            </section>
+            <!-- Botón de compra -->
+            <section class="buy-section">
+                <i class="fas fa-shopping-cart"></i>
+            </section>
+        </main>
+
+        <footer class="main-footer">
+            FOOTER
+        </footer>
+    </body>
+</html>
