638b72d65c7439347820dc3dc05726daa6416763
DigitalHouse <camiq_07@hotmail.com>
2019-10-16 11:09:38 -0300

    Arregle los iconos del footer.

--- common/footer.php
+++ common/footer.php
@@ -12,21 +12,13 @@
         <div class="flex-center">
 
           <!-- Facebook -->
-          <a class="socialMedia" href="https://www.facebook.com/">
-              <ion-icon class="logo-facebook" name="logo-facebook"></ion-icon>
-          </a>
+          <a class="socialMedia" href="https://www.facebook.com/"><ion-icon class="logo-facebook" name="logo-facebook"></ion-icon></a>
           <!-- Twitter -->
-          <a class="socialMedia" href="https://twitter.com/">
-              <ion-icon class="logo-twitter" name="logo-twitter"></ion-icon>
-          </a>
+          <a class="socialMedia" href="https://twitter.com/"><ion-icon class="logo-twitter" name="logo-twitter"></ion-icon></a>
           <!--Instagram-->
-          <a class="socialMedia" href="https://www.instagram.com/">
-              <ion-icon class="logo-instagram" name="logo-instagram"></ion-icon>
-          </a>
+          <a class="socialMedia" href="https://www.instagram.com/"><ion-icon class="logo-instagram" name="logo-instagram"></ion-icon></a>
           <!--Github-->
-          <a class="socialMedia" href="https://github.com/">
-              <ion-icon class="logo-github" name="logo-github"></ion-icon>
-          </a>
+          <a class="socialMedia" href="https://github.com/"><ion-icon class="logo-github" name="logo-github"></ion-icon></a>
         </div>
       </div>
       <!-- Grid column -->
@@ -38,7 +30,7 @@
   <!-- Footer Elements -->
 
   <!-- Copyright -->
-  <div class="footer-copyright text-center py-3">© 2019 Copyright:
+  <div class="footer-copyright text-center">© 2019 Copyright:
     <a href="./home.php"> Ecom.com</a>
   </div>
   <!-- Copyright -->
